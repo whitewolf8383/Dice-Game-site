a88d72afab4346a8fc2c67cda6d4bf0e7c9c06d1
whitewolf8383 <45803381+whitewolf8383@users.noreply.github.com>
2019-01-24 16:54:57 -0700

    modified for personal style

--- index_java.js
+++ index_java.js
@@ -1,28 +1,20 @@
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+var randomImageSource = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[2].setAttribute("src", randomImageSource2);
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-
-//If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  document.getElementById("pic2").style.visibility = "hidden";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  document.getElementById("pic1").style.visibility = "hidden";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
